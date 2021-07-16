2ffb882f064822dc2bd8180997490353db5f24c1
Bartosz Starczewski <alejary@gmail.com>
2021-07-16 21:21:55 +0200

    command

---
+++ bs-design-patterns/bs-design-patterns/command/CommandsManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace bs_design_patterns.command
+{
+    class CommandsManager
+    {
+        private Stack<ICommand> commands = new Stack<ICommand>();
+
+        public void Invoke(ICommand command)
+        {
+            if(command.CanExecute())
+            {
+                command.Execute();
+                this.commands.Push(command);
+            }
+        }
+
+        public void Undo()
+        {
+            if(this.commands.Any())
+            {
+                this.commands.Pop().Undo();
+            }
+        }
+    }
+}
---
+++ bs-design-patterns/bs-design-patterns/command/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bs_design_patterns.command
+{
+    interface ICommand
+    {
+        void Execute();
+        bool CanExecute();
+        void Undo();
+    }
+}
